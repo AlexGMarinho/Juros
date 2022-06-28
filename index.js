4e38867f45d1c0a3d9d03020a3b1be315816c4c7
AlexGMarinho <alexg.marinhoooo>
2022-06-28 19:33:57 -0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 import jr from 'readline-sync';
 
-console.log(`Aplicação de Juros:\n\n`);
+console.log(`Aplicação de Juros:\n`);
+
+let valor = jr.question(`Informe o valor devido: R$ `);
+let dias = jr.question(`Informe quantos dias se passaram desde o vencimento do boleto: `);
+
+console.log(`Valor original da dívida: R$ ${valor}`);
+console.log(`Días atrasados: ${dias}`);
