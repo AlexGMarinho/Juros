7e08be4d267ece3fea4b0920d8102c010dbe0c76
AlexGMarinho <alexg.marinhoooo>
2022-06-28 21:10:30 -0300

    index.js

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ import readlineSync from 'readline-sync';
 
 console.log(`Aplicação de Juros:\n`);
 
-let valor = readlineSync.question(`Informe o valor devido: R$ `);
-valor = Number(valor);
+let valor_divida = readlineSync.question(`Informe o valor devido: R$ `);
+valor_divida = Number(valor_divida);
 
-if (valor > 0) {
+if (valor_divida > 0) {
     let dias = readlineSync.question(`Informe quantos dias se passaram desde o vencimento do boleto: `);
 
     if (dias > 0) {
         if (dias > 15) {
             let juros = 10;
-            let valor_juros = (valor / 100) * juros;
-            console.log(`\nValor original da dívida: R$ ${valor}`);
+            let valor_juros = (valor_divida / 100) * juros;
+            console.log(`\nValor original da dívida: R$ ${valor_divida}`);
             console.log(`Días atrasados: ${dias}`);
             console.log(`Taxa de Juros: ${juros}%`);
-            console.log(`Valor total com juros: ${valor + valor_juros}`);
+            console.log(`Valor total com juros: ${valor_divida + valor_juros}`);
         } else {
             let juros = 5;
-            let valor_juros = (valor / 100) * 5;
-            console.log(`Valor original da dívida: R$ ${valor}`);
+            let valor_juros = (valor_divida / 100) * 5;
+            console.log(`Valor original da dívida: R$ ${valor_divida}`);
             console.log(`Días atrasados: ${dias}`);
             console.log(`Taxa de Juros: ${juros}%`);
-            console.log(`Valor total com juros: ${valor + valor_juros}`);
+            console.log(`Valor total com juros: ${valor_divida + valor_juros}`);
         }
     } else {
         console.log(`Você está em dia!`);
